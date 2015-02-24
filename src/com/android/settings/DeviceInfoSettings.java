bb017509dedfb6a45a6de012fee71b0e4ee58c2c
Louis Popi <theh2o64@gmail.com>
2015-02-24 19:16:13 +0100

    Settings: Add PAR version and CAF BRANCH to about phone
    Signed-off-by: Louis Popi <theh2o64@gmail.com>
    Signed-off-by: Louis Popi <theh2o64@gmai.com>
    Signed-off-by: Louis Popi <theh2o64@gmail.com>

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -84,6 +84,8 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
     private static final String PROPERTY_EQUIPMENT_ID = "ro.ril.fccid";
     private static final String KEY_DEVICE_FEEDBACK = "device_feedback";
     private static final String KEY_SAFETY_LEGAL = "safetylegal";
+    private static final String KEY_PAR_VERSION = "par_version";
+    private static final String KEY_PAR_BRANCH = "par_branch";
 
     static final int TAPS_TO_BE_A_DEVELOPER = 7;
     private static boolean mHideVersionName = false;
@@ -133,6 +135,10 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
         findPreference(KEY_BUILD_NUMBER).setEnabled(true);
         setValueSummary(KEY_QGP_VERSION, PROPERTY_QGP_VERSION);
         findPreference(KEY_KERNEL_VERSION).setSummary(getFormattedKernelVersion());
+	setValueSummary(KEY_PAR_VERSION, "ro.par.version");
+	setValueSummary(KEY_PAR_BRANCH, "ro.par.branch");
+	findPreference(KEY_PAR_VERSION).setEnabled(true);
+	findPreference(KEY_PAR_BRANCH).setEnabled(true);
 
         // Remove QGP Version if property is not present
         removePreferenceIfPropertyMissing(getPreferenceScreen(), KEY_QGP_VERSION,
