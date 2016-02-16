5c48e1ff32c35283c1fe83d338cdd6c225fc6c4e
Fang Yunong <yunong@codeaurora.org>
2016-02-16 14:03:06 +0800

    Settings: WFC: change persist properties to volatile
    Change system properties persist.sys.wificall.status.msg and
    persist.sys.wificall.ready to volatile because the wifi call
    status should not be kept after power cycle.
    
    Change-Id: I7b705100d55364353a1fb78c03837ceb7b83df17
    CRs-Fixed: 974974

--- src/com/android/settings/wificall/WifiCallingStatusControl.java
+++ src/com/android/settings/wificall/WifiCallingStatusControl.java
@@ -69,9 +69,8 @@ public class WifiCallingStatusControl extends BroadcastReceiver {
     public static final int WIFI_CALLING_ROVE_IN_THRESHOD = -75;
     public static final String ACTION_WIFI_CALL_READY_STATUS_CHANGE = "com.android.wificall.READY";
     public static final String ACTION_WIFI_CALL_READY_EXTRA = "com.android.wificall.ready.extra";
-    public static final String SYSTEM_PROPERTY_WIFI_CALL_READY = "persist.sys.wificall.ready";
-    public static final String SYSTEM_PROPERTY_WIFI_CALL_STATUS_MSG =
-            "persist.sys.wificall.status.msg";
+    public static final String SYSTEM_PROPERTY_WIFI_CALL_READY = "sys.wificall.ready";
+    public static final String SYSTEM_PROPERTY_WIFI_CALL_STATUS_MSG = "sys.wificall.status.msg";
     public static final String SYSTEM_PROPERTY_WIFI_CALL_TURNON = "persist.sys.wificall.turnon";
     private static final String SHAREDPREFERENCES_FILE_NAME = "MY_PERFS";
     private static final String SHAREDPREFERENCES_WIFI_CALL_SET = "is_first_set_wfc";
