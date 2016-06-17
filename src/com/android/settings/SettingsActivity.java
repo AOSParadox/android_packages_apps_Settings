9a70aa39355dbeac9cdb1ccd20308e36302d6523
xlxfoxxlx <joshfox87@gmail.com>
2016-06-17 04:33:59 -0400

    Add Substratum Dashboard Tile
    PureNexus Edits:
    *Squashed together
    
    Change-Id: Ifefbd5dc735c7689e6b38564288cc8ca9c82154a
    Signed-off-by: Louis Popi <theh2o64@gmai.com>
    Signed-off-by: Louis Popi <theh2o64@gmail.com>

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -1301,6 +1301,16 @@ public class SettingsActivity extends Activity
                     }
                 } else if (id == R.id.gestures_settings) {
                     removeTile = !GesturesSettings.supportsGestures(this);
+                } else if (id == R.id.substratum) {
+                  boolean supported = false;
+                  try {
+                    supported = (
+                      getPackageManager().getPackageInfo("projekt.substratum", 0).versionCode > 0);
+                  } catch (PackageManager.NameNotFoundException e) {
+                  }
+                  if (!supported) {
+                    removeTile = true;
+                  }
                 } else if (id == R.id.user_settings) {
                     boolean hasMultipleUsers =
                             ((UserManager) getSystemService(Context.USER_SERVICE))
